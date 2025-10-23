bc6c7f291480177dd680381e1cede02b1bb013d7
124Tainy <tainy.silva@escola.pr.gov.br>
2025-10-23 00:09:54 +0000

    Terminamos a atividade

--- app.js
+++ app.js
@@ -14,5 +14,11 @@ function criaCartao (categoria, pergunta, resposta) {
 </div>
 </div>
 `
+let respostaEstaVisivel = false
+function viraCartao(){
+    respostaestaVisivel = !respostaEstaVisivel
+    cartao.classList.toggle('active', respostaEstaVisivel)
+    cartao.addEventListener('click', viraCartao)
+}
 container.appendChild(cartao)
 }
